b15f9e125ab146b39c80037d85bf996040827c8e
kovsky <johan@developpeur-web.tech>
2020-03-14 09:12:49 +0100

    ok

--- src/components/blog/slices/Code.jsx
+++ src/components/blog/slices/Code.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/languages/prism'
-import { xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import styled from '@emotion/styled'
 
 const CodeSnippet = styled('div')`
   display: block;
   padding: 40px 0;
+  max-width: 1440px;
+  margin-left: auto;
+  margin-right: auto;
 `
 export default function Code({ slice }) {
   const codeString = slice.primary.code_snippet.text
   const language = slice.primary.code_snippet.text
   return (
     <CodeSnippet>
-      <SyntaxHighlighter language={language} style={xonokai}>
+      <SyntaxHighlighter language={language} style={dracula}>
         {codeString}
       </SyntaxHighlighter>
     </CodeSnippet>
