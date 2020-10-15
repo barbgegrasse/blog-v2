a9a48b7edb4f24affe3c71768babb35c143a7f43
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2020-10-15 09:17:08 +0200

    fix: update layout

--- src/styles/components/blog/listing.js
+++ src/styles/components/blog/listing.js
@@ -5,6 +5,7 @@ const BlogGrid = styled('section')`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 2.5em;
+  padding: 0 32px 0 64px;
 
   &:hover .card:not(:hover) {
     opacity: 0.6;
@@ -18,6 +19,7 @@ const BlogGrid = styled('section')`
 
   ${mediaQueries.mobile} {
     grid-template-columns: 1fr;
+    padding: 0 8px;
   }
 `
 
