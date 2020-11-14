677d55217a2a9d17c602522933465ab660edc6d2
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2020-11-14 20:17:58 +0100

    add content

---
+++ src/components/home/CMSsection.jsx
@@ -0,0 +1,183 @@
+import React from 'react'
+
+import {
+  ServicesWrapper,
+  Intro,
+} from '../../styles/components/home/blockPresentation'
+import { LineTitle } from '../../styles/common/title'
+
+const BlockPresentation = () => {
+  return (
+    <ServicesWrapper as="div">
+      <LineTitle as="h3">
+        Pourquoi faire appel à une développeur CMS ?
+      </LineTitle>
+      <Intro>
+        <p>
+          Aujourd’hui, pour la gestion efficace de vos contenus, vous devez
+          choisir un <strong>CMS</strong> (Content Management System). En effet,
+          une solution de gestion de contenu ou CMS vous permet à vous ou votre
+          équipe marketing d'éditer votre contenu très facilement. Cela signifie
+          qu'il n'est plus nécessaire de compter sur les développeurs web pour
+          modifier le contenu du site ; vos experts marketing peuvent ajouter du
+          contenu sans avoir besoin de connaître le HTML ou un quelconque code.
+        </p>
+      </Intro>
+      <Intro>
+        <LineTitle as="h4">Quel C.M.S utilisé ?</LineTitle>
+        <p>
+          Chaque C.M.S. a son mode de fonctionnement spécifique. Parcourons
+          ensemble leur mode de fonctionnement.
+        </p>
+        <h5>Prestashop</h5>
+        <p>
+          Selon la définition de Wikipédia, PrestaShop est un système de gestion
+          de contenu gratuit (CMS) et open source, conçu pour créer à partir de
+          zéro des boutiques en ligne de commerce électronique. Il permet la
+          création de boutiques en ligne, des petites entreprises aux grandes
+          entreprises. D'ailleurs, il dispose d'un large marché de thèmes pour
+          personnaliser la boutique et de plus de 5000 modules, gratuits et
+          payants, avec lesquels sont adaptées les fonctionnalités propres de
+          l'outil.
+        </p>
+        <p>
+          Même avec sa gratuité, vous ne pouvez pas l’utiliser comme logiciel de
+          service. Ainsi, avant son installation, il faut au préalable le
+          télécharger sur le site et le copier le document téléchargé chez votre
+          hébergeur. Une fois ce processus terminé, vous pouvez exécuter
+          l’installation qui peut être parfois long.
+        </p>
+        <p>
+          A la fin du processus d’installation, vous procédez à son paramétrage.
+          Voici quelques paramétrages utiles à faire. Cette liste n’est pas
+          exhaustive. Vous devez :
+        </p>
+        <ul>
+          <li>
+            Implanter votre style par la mise en place du thème de votre choix ;
+          </li>
+          <li>
+            Procéder à la configuration des éléments de votre boutique (Monnaie,
+            lieux et frais de livraison, la gestion de plusieurs langues) ;
+          </li>
+          <li>Procéder à l’optimisation SEO avec le mot clé ;</li>
+          <li>
+            Procéder à l’ajout des pages de présentation de l’entreprise,
+            marques, contact, etc. ;
+          </li>
+          <li>Création de votre catalogue de produits.</li>
+        </ul>
+        <p>Comme avantages, vous avez :</p>
+        <ul>
+          Un logiciel gratuit ;
+          <li>Un logiciel qui est leader sur le marché francophone ;</li>
+          <li>
+            Un logiciel qui est complet et adapté aux boutiques en ligne et qui
+            couvre presque tous les besoins ;
+          </li>
+          <li>
+            Un logiciel en constante évolution, mais une technologie parfois
+            vieillissante.
+          </li>
+        </ul>
+        <p>
+          Notons que la mise en place de prestashop peut être assez long. Aussi,
+          sa mise à jour est souvent difficile et nécessite l’expertise d’un
+          prestataire spécialisé.
+        </p>
+      </Intro>
+      <Intro>
+        <h5>Drupal</h5>
+        <p>
+          Selon Wikipédia Drupal est un système et un framework de gestion de
+          contenu (appelé. CMF content management framework) écrit en PHP est
+          utilisé par plus d'un million de sites Web.
+        </p>
+        <p>
+          Etant dans la même catégorie que les CMS open source, Drupal est un
+          logiciel gratuit. Aussi, Il est proposé directement sur les interfaces
+          d’administration des hébergeurs. Aussi, vous avez un choix parmi un
+          large éventail de thèmes afin de personnaliser votre site. À ce jour,
+          il en existe plus de 2000 plugins gratuits et payants.
+        </p>
+        <p>
+          Le fonctionnement du CMS Drupal fait appel à l’installation de
+          plusieurs modules.{' '}
+        </p>
+        <p>Cette solution de base permet alors :</p>
+        <ul>
+          <li>De personnaliser votre design ;</li>
+          <li>D’éditer les pages ;</li>
+          <li>D’ajouter et de gérer un blog ;</li>
+          <li>
+            D’insérer les formulaires, les petites applications (widgets), etc.
+          </li>
+        </ul>
+        <p>
+          En bref, Drupal est facilement personnalisable et expansible. Ainsi,
+          il possède de nombreux modules et une rubrique blog, e-commerce et des
+          fonctionnalités poussées telles que la{' '}
+          <strong>
+            gestion de sondages, de menu avancé, des modules forums de
+            discussion, réseaux sociaux, des outils de modification graphiques.
+          </strong>
+        </p>
+        <p>
+          Malgré la facilité à l’installer, notons que le CMS Drupal ne peut pas
+          permettre à un débutant de créer rapidement un site internet. Il lui
+          faudra du temps et des efforts supplémentaires pour s’adapter à son
+          interface complexe.
+        </p>
+        <p>
+          De même, il est plus adapté au professionnel avec des connaissances en
+          informatique.
+        </p>
+      </Intro>
+      <Intro>
+        <h5>Magento</h5>
+        <p>
+          Selon Wikipédia Magento est une plateforme de commerce électronique
+          appartenant à Adobe. De plus, il existe deux versions: Magento Open
+          Source, la version open source gratuite écrite en PHP et Magento
+          Commerce, la version cloud payante.
+        </p>
+        <p>
+          Pour l’installation de Magento, voici alors quelques étapes à suivre :
+        </p>
+        <ul>
+          <li>
+            Procéder au téléchargement des fichiers d’installation de Magento ;
+          </li>
+          <li>Procéder au choisir d’un serveur compatible avec Magento ;</li>
+          <li>Chercher et trouver le nom de domaine ;</li>
+          <li>
+            Utiliser un client FTP pour l’installation de Magento sur le serveur
+            ;
+          </li>
+          <li>Procéder à l’installation du fichier Magento sur le serveur ;</li>
+          <li>
+            Procéder à l’installation de Magento et configurer le e-boutique ;
+          </li>
+          <li>Enfin, Accéder au panneau d’administration de Magento.</li>
+        </ul>
+        <p>
+          Avec sa rapidité d’exécution et l’utilisation de la fonction
+          multidevise, Magento peut aussi permettre la vente d’une importante
+          quantité de produits. Vous pouvez aussi configurer plusieurs boutiques
+          en ligne. Il dispose des fonctionnalités surtout pour les animations
+          marketing et permet de faire la gestion des catalogues de produits
+          assez variés.
+        </p>
+        <p>
+          Magento offre ainsi un gain de temps important à cause de son
+          back-office qui lui permet d’effectuer des mises à jour groupées. Du
+          côté SEO, le CMS Magento permet à l’aide de paramétrages précis de
+          remonter facilement des sites e-commerce parmi les premiers résultats
+          des SERPs.il est très apprécié des développeurs.
+        </p>
+      </Intro>
+    </ServicesWrapper>
+  )
+}
+
+export default BlockPresentation
